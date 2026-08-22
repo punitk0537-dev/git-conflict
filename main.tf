1945f42e17d1d2991316480704436b1e5c52866c
Punit Kumar <punitk0537@gmail.com>
2026-08-23 00:51:10 +0530

    my first commit

---
+++ main.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=5.0.0"
+    }
+  }
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+}
+resource "azurerm_resource_group" "rg" {
+  name     = "punit-rg"
+  location = "West Europe"
+}
